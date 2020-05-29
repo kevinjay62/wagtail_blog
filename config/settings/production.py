f33b83e0f1c486cfa1fb13b7b564bdebf07a3e1e
Kevin Jay <kevin@kjay.net>
2020-05-29 12:27:40 -0500

    modify production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,21 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRET_KEY
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["192.168.10.65", "localhost", "127.0.0.1"]
+
+ADMINS = (
+    ('Kevin Jay', 'kevin@kjay.net'),
+)
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ["192.168.10.65", "localhost", "127.0.0.1"]
+
 
 try:
     from .local import *
